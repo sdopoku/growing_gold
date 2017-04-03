13b94ea571d632f34bb3630f7e438ff045cd8667
sdopoku <david@adoorelabs.com>
2017-04-03 07:39:53 +0000

    clean R code and file names to work independently.

--- datasets/cleaning_code/ghana_oil_palm_actual_2003_2012.R
+++ datasets/cleaning_code/ghana_oil_palm_actual_2003_2012.R
@@ -1,41 +1,41 @@
-
-
 # Load libraries
-library(dplyr) # functions to manipulate data
 library(tidyr) # functions to clean data
 library(readr) # functions to read and write data
+library(dplyr) # functnions to manipulate data
 
 # Set working directory to source file location
 setwd(".")
 
 # Get data
-oil_palm <- read_csv("ghana_oil_palm_2003_2012_original.csv")
+# Data source: Tabula PDF scraping of page 20 of Ghana Ministry of Agriculture
+#              Facts and Figures 2012 (file: /datasets/sources/mofa_agriculture_facts_and_figures_2012.pdf ) 
+
+dirty_oil_palm <- read_csv("../dirty_data/ghana_oil_palm_2003_2012_dirty.csv")
 
 # View data
-glimpse(oil_palm)
-View(oil_palm)
-
+glimpse(dirty_oil_palm)
+View(dirty_oil_palm)
 
 # Remove empty rows/observations
-oil_palm <- na.omit(oil_palm)
+dirty_oil_palm <- na.omit(dirty_oil_palm)
 
 # Remove Sub-Total(row 6) and TOTAL (row 9) rows
-oil_palm <- oil_palm[-c(6, 9),]
+dirty_oil_palm <- dirty_oil_palm[-c(6, 9),]
 
-# Change company abbreviations to full names
-oil_palm$Company <- c("Ghana Oil Palm Development Company Limited",
-                      "Benso Oil Palm Production Limited",
-                      )
+# Clean up Medium Scale Mills and Small-Scall and Other Private Holdings names
+dirty_oil_palm <- dirty_oil_palm %>%
+  mutate(Company = replace(Company, Company == "Medium\nScale Mills", "Medium-Scale Mills")) %>%
+  mutate(Company = replace(Company, Company == "Small-Scale\nand Other\nPrivate\nHoldings", "Small-Scale and Private"))
+
 
 # Turn wide data into long data
-long_oil_palm <- oil_palm %>% 
+clean_oil_palm <- dirty_oil_palm %>% 
   gather(Year, `Production(mt)`, `2003` : `2012`) %>%
   arrange(Company)
 
-
 # View data
-View(long_oil_palm)
-
+glimpse(clean_oil_palm)
+View(clean_oil_palm)
 
 # Write data to CSV file
-write_csv(long_oil_palm, "ghana_oil_palm_2003_2012_cleaned.csv")
+write_csv(clean_oil_palm, "../clean_data/ghana_oil_palm_2003_2012_clean.csv")
